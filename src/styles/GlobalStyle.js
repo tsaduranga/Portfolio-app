1bb427378e572c042b13b94ec9eaf4aabc4fea58
tsaduranga <tsaduranga@gmail.com>
2021-09-27 02:16:52 +0530

    this is initial commit

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+        *{
+            padding:0;
+            margin : 0;
+            box-sizing : boarder-box;
+            font-size : 1.2rem;
+        }
+
+
+        body{
+            background-color : #333;
+        }
+`
+
+export default GlobalStyle
